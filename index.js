bf7c10aaa2764d375e041c301b2aac9ca003e9ff
TrevorZylks <trevorzylks@gmail.com>
2021-06-27 08:45:02 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 12;
+const num4 = 8;
+const mod = num3 % num4;
+
+const max = Math.max(1,2,20);
